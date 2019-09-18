3d1fd816a778da955be7dc7517f31481e021f933
chris-cb <54929607+chris-cb@users.noreply.github.com>
2019-09-18 17:31:45 +0200

    updated ApiApiController to return JSON in case of error

--- modules/swagger-promote-core/src/main/java/com/axway/apim/swagger/config/ConfigHandlerFactory.java
+++ modules/swagger-promote-core/src/main/java/com/axway/apim/swagger/config/ConfigHandlerFactory.java
@@ -1,6 +1,5 @@
 package com.axway.apim.swagger.config;
 
-import java.io.File;
 import java.io.InputStream;
 
 import com.axway.apim.lib.AppException;
@@ -8,11 +7,10 @@ import com.axway.apim.lib.AppException;
 public class ConfigHandlerFactory {
 
 	public static ConfigHandlerInterface getConfigHandler (Object apiConfig, Object apiDefinition, String stage, boolean orgAdminUsed) throws AppException {
-		if(apiConfig instanceof String && 
-				(new File((String)apiConfig).exists()) || ((String)apiConfig).startsWith("..")) {
-			return new FileConfigHandler((String)apiConfig, (String)apiDefinition, stage, orgAdminUsed);
+		if(apiConfig instanceof InputStream) {
+      return new StreamConfigHandler((InputStream)apiConfig, (InputStream)apiDefinition, stage, orgAdminUsed);
 		} else {
-			return new StreamConfigHandler((InputStream)apiConfig, (InputStream)apiDefinition, stage, orgAdminUsed);
+      return new FileConfigHandler((String)apiConfig, (String)apiDefinition, stage, orgAdminUsed);
 		}
 	}
 }
